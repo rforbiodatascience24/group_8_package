51c41df12f789c9a1a86e8fb9966efa131b768a1
ceciliemoellerjensen <cecilie.moeller.jensen@gmail.com>
2024-10-29 09:37:26 +0100

    standard_codon_table creation

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' standard_codon_table
+#'
+#' translations of codons to amino acids
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"standard_codon_table"
