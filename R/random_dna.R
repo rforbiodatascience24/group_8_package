1c31e5177060e6b17fd60e0a76810924ff0a434d
DTU-MVH <s215073@dtu.dk>
2024-10-29 10:37:53 +0100

    deleting hello world and creating tests

--- R/random_dna.R
+++ R/random_dna.R
@@ -11,7 +11,7 @@
 random_dna <- function(lenght_of_dna){
 
   #Creating a vector of length lenght_of_dna randomly chosen from the nucleic bases.
-  sample_dna_vector <- sample(c("A", "T", "G", "C"), size = name_me2, replace = TRUE)
+  sample_dna_vector <- sample(c("A", "T", "G", "C"), size = lenght_of_dna, replace = TRUE)
 
   #concatenates the vector with nothing between the elements
   random_dna_string <- paste0(sample_dna_vector, collapse = "")
