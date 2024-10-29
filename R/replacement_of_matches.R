4abf144ddab6ae1908755b7feda0e0484ee942bc
Juliehaugaard <s201523@dtu.dk>
2024-10-29 10:07:12 +0100

    made manual

---
+++ R/replacement_of_matches.R
@@ -0,0 +1,17 @@
+#' Replace T with U
+#'
+#' @param DNA_sequence
+#'
+#' @return RNA sequence
+#' @export
+#'
+#' @examples
+#' replace_T_with_U(GATTA)
+#' return(GAUUA)
+
+replace_T_with_U <- function(DNA_sequence){
+  RNA_sequence <- gsub("T", "U", DNA_sequence)
+  return(RNA_sequence)
+}
+
+
