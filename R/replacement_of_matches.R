6e4223d64b6a13b024a0afaa37439dd40405a854
Juliehaugaard <s201523@dtu.dk>
2024-10-29 10:29:38 +0100

    Ændret til string

--- R/replacement_of_matches.R
+++ R/replacement_of_matches.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 #' @examples
-#' replace_T_with_U(GATTA)
+#' replace_T_with_U(DNA_sequence = "GATTA")
 
 replace_T_with_U <- function(DNA_sequence){
   RNA_sequence <- gsub("T", "U", DNA_sequence)
